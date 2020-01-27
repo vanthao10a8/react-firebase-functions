adf28c78158689af7cbe8751caed5e4a7dfb890e
Thao Nguyen <vanthao10a8@gmail.com>
2020-01-27 22:00:35 +0700

    modify all code

---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: "AIzaSyCNglAaGTiOiAOCikEttseo4uRaKnBw1Ns",
+  authDomain: "socialape-stu.firebaseapp.com",
+  databaseURL: "https://socialape-stu.firebaseio.com",
+  projectId: "socialape-stu",
+  storageBucket: "socialape-stu.appspot.com",
+  messagingSenderId: "559553866876",
+  appId: "1:559553866876:web:a4d125936e4f6cbb39adf9",
+  measurementId: "G-LW8Y088D3Y"
+};
